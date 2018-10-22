8ae0ac969a5bfa5b5eae031e82251bf25b3e95b0
Vincent Laporte <Vincent.Laporte@fondation-inria.fr>
2018-10-22 13:03:13 +0000

    nat

--- src/nat.v
+++ src/nat.v
@@ -7,3 +7,11 @@ Inductive nat :=
 Register nat as num.nat.type.
 Register O as num.nat.O.
 Register S as num.nat.S.
+
+Declare Scope nat_scope.
+
+Arguments S _%nat_scope.
+
+Notation "0" := O : nat_scope.
+Notation "1" := (S 0) : nat_scope.
+Notation "2" := (S 1) : nat_scope.
