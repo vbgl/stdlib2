6a4419e6b0a64d3f5b2b1065db70e99155825b32
Vincent Laporte <Vincent.Laporte@fondation-inria.fr>
2019-03-22 15:13:08 +0000

    [WIP] on nat & seq

--- src/nat.v
+++ src/nat.v
@@ -1,4 +1,5 @@
 Require Import prelude ssreflect datatypes decimal.
+Import equality prop.
 
 Inductive nat :=
 | O
@@ -9,8 +10,35 @@ Register O as num.nat.O.
 Register S as num.nat.S.
 
 Declare Scope nat_scope.
-
+Delimit Scope nat_scope with N.
+Local Open Scope nat_scope.
+Bind Scope nat_scope with nat.
 Arguments S _%nat_scope.
+
+Notation succn := S.
+
+Definition predn (n: nat) : nat :=
+  if n is succn n' then n' else n.
+
+Notation "n .+1" := (succn n) (at level 2, left associativity,
+  format "n .+1") : nat_scope.
+Notation "n .+2" := n.+1.+1 (at level 2, left associativity,
+  format "n .+2") : nat_scope.
+Notation "n .+3" := n.+2.+1 (at level 2, left associativity,
+  format "n .+3") : nat_scope.
+Notation "n .+4" := n.+2.+2 (at level 2, left associativity,
+  format "n .+4") : nat_scope.
+
+Notation "n .-1" := (predn n) (at level 2, left associativity,
+  format "n .-1") : nat_scope.
+Notation "n .-2" := n.-1.-1 (at level 2, left associativity,
+  format "n .-2") : nat_scope.
+
+Lemma nat_eqI (m n: nat) :
+  m = n ->
+  if m is m'.+1 then if n is n'.+1 then m' = n' else False
+  else if n is O then True else False.
+Proof. by move => ->; case: n. Qed.
 
 Fixpoint tail_add (m n: nat) : nat :=
   if m is S m' then tail_add m' (S n) else n.
