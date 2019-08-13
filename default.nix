7756cae2fd368368cac1a08eb5c03f2d680fee28
Vincent Laporte <Vincent.Laporte@fondation-inria.fr>
2019-08-13 13:22:18 +0000

    Use custom Coq

--- default.nix
+++ default.nix
@@ -1,8 +1,19 @@
 {
-  pkgs ? import <nixpkgs> {}
+pkgs ? import (fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/bc9df0f66110039e495b6debe3a6cda4a1bb0fed.tar.gz";
+  sha256 = "0y2w259j0vqiwjhjvlbsaqnp1nl2zwz6sbwwhkrqn7k7fmhmxnq1";
+}) {}
+
 }:
 
-let coq = import (fetchTarball https://github.com/coq/coq-on-cachix/tarball/master) {}; in
+#let coq = import (fetchTarball https://github.com/coq/coq-on-cachix/tarball/master) {}; in
+
+let coq = import (fetchTarball {
+    url = https://github.com/vbgl/coq/archive/ltac2+noinit.tar.gz;
+    sha256 = "0bcl5mra9xb4y9xkmlrdfm3k3if753dpdnj29ynb24bk8cagi1dz";
+  }
+) {};
+in
 
 let callPackage = pkgs.newScope { inherit coq; }; in
 
